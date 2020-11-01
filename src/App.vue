16f951cb361b2915b1115fa45d227ca49f62f13f
Carlos Revilla <clro26.k@gmail.com>
2020-11-01 01:15:04 +0100

    ✨ Header component creation

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
+    <Header />
     <List />
   </div>
 </template>
 
 <script>
 import List from './components/List.vue'
+import Header from './components/Header'
 
 export default {
   name: 'App',
   components: {
-    List
+    List,
+    Header
   }
 }
 </script>
