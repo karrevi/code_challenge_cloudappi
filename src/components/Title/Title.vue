4a14d524abee4ad7f402a7cc2d8399f8e40483e5
Carlos Revilla <clro26.k@gmail.com>
2020-11-01 22:50:04 +0100

    ✨Build file for title and add style components to header title

---
+++ src/components/Title/Title.vue
@@ -0,0 +1,20 @@
+<template>
+  <title-content>{{ title }}</title-content>
+</template>
+
+<script>
+import { TitleContent } from './style'
+
+export default {
+  name: 'Title',
+  components: {
+    TitleContent
+  },
+  props: {
+    title: String
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/Title/style.js
@@ -0,0 +1,8 @@
+import styled from 'vue-styled-components'
+
+export const TitleContent = styled.h1`
+font-display: sans-serif;
+text-transform: uppercase;
+display:block;
+text-align:center;
+`
