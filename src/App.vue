e7d44cd94b95b0fe9e88174ee157d9b23eec6e69
Carlos Revilla <clro26.k@gmail.com>
2020-11-01 01:25:49 +0100

    ✨Footer component creation

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
   <div id="app">
     <Header />
     <List />
+    <Footer />
   </div>
 </template>
 
 <script>
 import List from './components/List.vue'
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 export default {
   name: 'App',
   components: {
     List,
-    Header
+    Header,
+    Footer
   }
 }
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer>
+    <p>{{ text }}</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      text: 'Created with 💚 by Carlos, 2020'
+    }
+  }
+}
+</script>
