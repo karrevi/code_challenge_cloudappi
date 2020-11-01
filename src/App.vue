e00e297d674f1bcd6f459ffcef3d12d81257c28e
Carlos Revilla <clro26.k@gmail.com>
2020-11-01 22:56:09 +0100

    ✨ Add title component and integrate to header

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <Header />
+    <Title title="Lista de Usuarios" />
     <List />
+    <Card />
     <Footer />
   </div>
 </template>
@@ -10,13 +12,28 @@
 import List from './components/List.vue'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import Card from './components/Card'
+import Title from './components/Title/Title'
 
 export default {
   name: 'App',
   components: {
     List,
+    Card,
+    Title,
     Header,
     Footer
   }
 }
 </script>
+
+<style>
+body,
+ul,
+#app {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  text-decoration: none;
+}
+</style>
