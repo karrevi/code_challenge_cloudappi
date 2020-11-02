3172e4548165195cae10eff4fb6f36d76d289920
Carlos Revilla <clro26.k@gmail.com>
2020-11-02 22:27:37 +0100

    ✨ Incorporate props, subtitle and style

---
+++ src/components/Header/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <header-style>
+    <Title title="Lista de Usuarios" subtitle="Code Challenge CloudAppi" />
+  </header-style>
+</template>
+
+<script>
+import { HeaderStyle } from './style'
+import Title from '../Title/Title'
+
+export default {
+  name: 'Header',
+  components: {
+    HeaderStyle,
+    Title
+  },
+  props: {
+    title: String,
+    subtitle: String
+  }
+}
+</script>
---
+++ src/components/Header/style.js
@@ -0,0 +1,10 @@
+import styled from 'vue-styled-components'
+
+export const HeaderStyle = styled.header`
+background: rgb(255, 255 255);
+height: 80px;
+align-self: center;
+top: 0px;
+width: 100%;
+border-bottom: 1px solid rgba(0, 0, 0, 0.05);
+`
