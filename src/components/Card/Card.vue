bc0449e82469edbe1b0a50596362a77d577de475
Carlos Revilla <clro26.k@gmail.com>
2020-11-02 22:31:07 +0100

    ✨ Design card component and incorporate props

---
+++ src/components/Card/Card.vue
@@ -0,0 +1,25 @@
+<template>
+  <card-wrapper>
+    <li>
+      <h2>{{ item }}</h2>
+      <h3>{{ user }}</h3>
+      <p>{{ email }}</p>
+    </li>
+  </card-wrapper>
+</template>
+
+<script>
+import { CardWrapper } from './style'
+
+export default {
+  name: 'Card',
+  components: {
+    CardWrapper
+  },
+  props: {
+    item: Number,
+    email: String,
+    user: String
+  }
+}
+</script>
