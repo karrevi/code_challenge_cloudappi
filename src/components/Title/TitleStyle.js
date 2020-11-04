47ab41b5b11e51ec4f8b90da6546d227bc3cef75
Carlos Revilla <clro26.k@gmail.com>
2020-11-04 14:54:16 +0100

    💄 Styles to text structure

---
+++ src/components/Title/TitleStyle.js
@@ -0,0 +1,21 @@
+import styled from 'vue-styled-components'
+
+export const TitleContent = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-content: center;
+    align-items:center;
+
+h2, p {
+    color: #FFFFFF;
+    font-family: 'Noto Sans', sans-serif;
+    font-size: 2em;
+    font-weight: 700;
+}
+h5 {
+    color: orange;
+    font-size: 1.5em;
+    font-family: 'Parisienne', cursive;
+}
+`
