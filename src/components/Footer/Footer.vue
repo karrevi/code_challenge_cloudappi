836df7c29b51af8b7f4553b95204eaa13d74f453
Carlos Revilla <clro26.k@gmail.com>
2020-11-04 13:46:40 +0100

    ✨ Footer update and layout added

---
+++ src/components/Footer/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+  <designFooter>
+    <Title textfooter="Creado por Carlos Revilla" />
+  </designFooter>
+</template>
+
+<script>
+import Title from '../Title/Title'
+import { designFooter } from './FooterStyle'
+
+export default {
+  name: 'Footer',
+  components: {
+    Title,
+    designFooter
+  },
+  props: {
+    textfooter: String
+  }
+}
+</script>
