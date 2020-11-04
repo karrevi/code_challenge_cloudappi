dbc223329310a4117ad8df4c1f3d816408fd1f94
Carlos Revilla <clro26.k@gmail.com>
2020-11-04 13:38:27 +0100

    🔨 Import bootstrap packages

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
 import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 import store from './store/index'
 
 Vue.config.productionTip = false
+Vue.use(BootstrapVue)
 
 new Vue({
   store,
