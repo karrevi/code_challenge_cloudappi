c4183cb74a00ae7e856791eba385dbf6d2f27314
Carlos Revilla <clro26.k@gmail.com>
2020-11-04 13:48:14 +0100

    ✨ Header update and layout added

---
+++ src/components/Header/HeaderStyle.js
@@ -0,0 +1,13 @@
+import styled from 'vue-styled-components'
+
+export const HeaderStyle = styled.header`
+display: flex;
+justify-content: center;
+align-content: center;
+align-items:center;
+background: #03848E;
+height: 6rem;
+top: 0px;
+width: 100%;
+box-shadow: 0px 0px 10px #02636A
+`
