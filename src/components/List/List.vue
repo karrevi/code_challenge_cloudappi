5b7ae4b4473df6ed9b28ea1fab893f70309ec830
Carlos Revilla <clro26.k@gmail.com>
2020-11-04 14:02:34 +0100

    ✨Incorporate component card, use props and state via vuex

---
+++ src/components/List/List.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <ListStyle>
+      <Card
+        v-for="user in users"
+        :key="user.id"
+        :item="user.id"
+        :user="user.name"
+        :email="user.email"
+      ></Card>
+    </ListStyle>
+  </div>
+</template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex'
+import Card from '../Card/Card'
+import { ListStyle } from './ListStyle'
+
+export default {
+  name: 'List',
+  components: {
+    Card,
+    ListStyle
+  },
+  mounted () {
+    this.LOAD_USERS()
+  },
+  computed: {
+    ...mapGetters(['users'])
+  },
+  methods: {
+    ...mapActions(['LOAD_USERS'])
+  }
+}
+</script>
