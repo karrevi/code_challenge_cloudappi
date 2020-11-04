9c6f93958b4afb64358b5c8954568056351b68fe
Carlos Revilla <clro26.k@gmail.com>
2020-11-04 18:46:40 +0100

    💄 Modify styles and remove unnecessary component

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,12 @@
   <div id="app">
     <Header />
     <List />
-    <Card />
     <Footer />
   </div>
 </template>
 
 <script>
 import Header from './components/Header/Header'
-import Card from './components/Card/Card'
 import List from './components/List/List'
 import Footer from './components/Footer/Footer'
 
@@ -17,7 +15,6 @@ export default {
   name: 'App',
   components: {
     List,
-    Card,
     Header,
     Footer
   }
@@ -31,6 +28,13 @@ export default {
   box-sizing: border-box;
 }
 #app {
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+  justify-content: space-between;
+  min-height: 100vh;
+  min-width: 100vw;
+  position: relative;
   font-family: "Montserrat", sans-serif;
   text-align: center;
 }
